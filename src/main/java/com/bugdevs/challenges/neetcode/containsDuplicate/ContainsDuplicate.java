8abb7b577938b3c76ec5dc182ae8b6bd2631fc73
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2025-05-13 21:37:42 -0500

    refactor: improve readability of hasDuplicate method

--- src/main/java/com/bugdevs/challenges/neetcode/containsDuplicate/ContainsDuplicate.java
+++ src/main/java/com/bugdevs/challenges/neetcode/containsDuplicate/ContainsDuplicate.java
@@ -10,7 +10,11 @@ public class ContainsDuplicate {
     public boolean hasDuplicate(int[] nums) {
         Set<Integer> seen = new HashSet<>();
 
-        for (int num : nums) if (!seen.add(num)) return true;
+        for (int num : nums) {
+            if (!seen.add(num)) {
+                return true;
+            }
+        }
 
         return false;
     }
